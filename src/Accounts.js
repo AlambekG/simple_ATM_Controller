10b67570c415dc903cd1db553260b6e71814a04a
Alambek Gulamidinov <alan.gulamidinov@gmail.com>
2024-11-24 14:35:03 +0900

    separate accounts data

---
+++ src/Accounts.js
@@ -0,0 +1,18 @@
+const accounts = {
+  '1234567890': {
+    pin: '1234',
+    accounts: {
+      checking: 1000,
+      savings: 5000,
+    },
+  },
+  '0987654321': {
+    pin: '4321',
+    accounts: {
+      checking: 5000,
+      savings: 1000,
+    },
+  },
+}
+
+module.exports = accounts;
--- src/BankService.js
+++ src/BankService.js
@@ -1,21 +1,4 @@
-// const accounts = require('./Accounts');
-
-const accounts = {
-  '1234567890': {
-    pin: '1234',
-    accounts: {
-      checking: 1000,
-      savings: 5000,
-    },
-  },
-  '0987654321': {
-    pin: '4321',
-    accounts: {
-      checking: 5000,
-      savings: 1000,
-    },
-  },
-}
+const accounts = require('./Accounts');
 
 class BankService {
   constructor() {
